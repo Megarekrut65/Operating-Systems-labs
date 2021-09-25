83130d52002a350eabf598eb91d7b6e1fd7d5b26
Megarekrut65 <bandalak28@gmail.com>
2021-09-25 23:07:22 +0300

    Create main

---
+++ cmake-build-debug/CMakeFiles/Operating_Systems_labs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Operating_Systems_labs.dir/Labs/Lab1/main.cpp.obj"
+  "Operating_Systems_labs.exe"
+  "Operating_Systems_labs.exe.manifest"
+  "Operating_Systems_labs.lib"
+  "Operating_Systems_labs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Operating_Systems_labs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
